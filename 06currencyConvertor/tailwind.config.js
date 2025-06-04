a2d50149d6226d8fea8f7cc82a34d8973247bf66
22P81A0534 <konamonichiranjeevi@gmail.com>
2025-06-04 20:50:30 +0530

    initial commit

---
+++ 06currencyConvertor/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // for React projects
+    "./public/index.html"         // optional, if you use HTML in public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
